2182251125472239fbd6325fef092038c06da163
Silas Almeida <silas.ms.almeida@gmail.com>
2021-02-03 18:23:25 -0300

    Setada a orientação do app para somente Portrait Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pondera/screens/initial-screen/initial-screen.dart';
 import 'package:pondera/utils/materialColors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MyApp());
 }
 
