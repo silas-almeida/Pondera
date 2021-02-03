839a97eeee3d62f92ba3ca7630f46112fa8c2d0e
Silas Almeida <silas.ms.almeida@gmail.com>
2021-02-03 18:16:18 -0300

    Projeto linkado ao Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pondera/screens/initial-screen/initial-screen.dart';
 import 'package:pondera/utils/materialColors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore.instance
+        .collection('colection')
+        .doc('teste')
+        .set({'text': 'Silas'});
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pondera',
